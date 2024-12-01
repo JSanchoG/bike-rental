ba87b7bb11cc48e582d472ffacc2effdfd032dd2
Jakub Sancho Gożuk <steamgozuk@gmail.com>
2024-12-02 00:12:26 +0100

    Implementacja funkcji: wczytanie wynajmów, anulowanie wynajmu
    Globalizacja zmiennych: ścieżek do plików i zmiennej walutowej

--- bike_rental.py
+++ bike_rental.py
@@ -3,6 +3,9 @@ import os
 from datetime import datetime
 import smtplib
 
+FILE_RENTALS_PATH = "data/rentals.json"
+CURRENCY = "PLN"
+
 def calculate_cost(rental_duration:int):
     '''
     Oblicza koszt wynajmu
@@ -15,21 +18,18 @@ def save_rental(rental:dict):
     '''
     Zapisuje dane wynajmu do pliku JSON
     '''
-
-    file_path = "data/rentals.json"
-    
-    # Check for file_path file, create as empty list if doesn't exist
+    # Check for {FILE_RENTALS_PATH} file, create as empty list if doesn't exist
     # NOTE: Doesn't work with an empty file, has to include an empty array, too lazy to fix zz
-    if not os.path.exists(file_path):
-        with open(file_path, 'w') as file:
+    if not os.path.exists(FILE_RENTALS_PATH):
+        with open(FILE_RENTALS_PATH, 'w') as file:
             json.dump([], file)
 
-    with open(file_path, 'r') as file:
+    with open(FILE_RENTALS_PATH, 'r') as file:
         rentals = json.load(file)
 
     rentals.append(rental)
 
-    with open(file_path, 'w') as file:
+    with open(FILE_RENTALS_PATH, 'w') as file:
         json.dump(rentals, file, indent=4)
 
 def rent_bike(customer_name:str, rental_duration:int):
@@ -51,11 +51,35 @@ def load_rentals():
     '''
     Wczytuje wynajmu z pliku JSON
     '''
+    if os.path.exists(FILE_RENTALS_PATH):
+        with open(FILE_RENTALS_PATH, 'r') as file:
+            rentals = json.load(file)
+            print()
+            for rental in rentals:
+                # print(rental)
+                print(f"Customer: {rental['customer_name']}")
+                print(f"Wynajem na {rental['rental_duration']} godz.")
+                print(f"Koszt: {rental['cost']} {CURRENCY}")
+                print(f"Data wynajmu: {rental['time']['formatted']}")
+                print()
+    else:
+        print("Brak zapisanych wynajmów.")
 
-def cancel_rental(customer_name):
+def cancel_rental(customer_name:str):
     '''
     Usuwa wynajem
     '''
+    if os.path.exists(FILE_RENTALS_PATH):
+        with open(FILE_RENTALS_PATH, 'r') as file:
+            rentals = json.load(file)
+        
+        rentals = [rental for rental in rentals if rental['customer_name'] != customer_name]
+
+        with open(FILE_RENTALS_PATH, 'w') as file:
+            json.dump(rentals, file, indent=4)
+        print(f"Wynajem dla {customer_name} został anulowany.")
+    else:
+        print("Brak zapisanych wynajmów.")
 
 def send_rental_invoice_email(customer_email, rental_details):
     '''
