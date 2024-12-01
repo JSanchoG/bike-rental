462ef191a865419b370d8a3e2aef313dd9c48308
Jakub Sancho Gożuk <steamgozuk@gmail.com>
2024-12-01 23:48:56 +0100

    Struktura plików, szkielet funkcji, import bibliotek

---
+++ bike_rental.py
@@ -0,0 +1,39 @@
+import json
+import os
+from datetime import datetime
+import smtplib
+
+def calculate_cost(rental_duration):
+    '''
+    Oblicza koszt wynajmu
+    '''
+
+def save_rental(rental):
+    '''
+    Zapisuje dane wynajmu do pliku JSON
+    '''
+
+def rent_bike(customer_name, rental_duration):
+    '''
+    Dodaje wynajem roweru
+    '''
+
+def load_rentals():
+    '''
+    Wczytuje wynajmu z pliku JSON
+    '''
+
+def cancel_rental(customer_name):
+    '''
+    Usuwa wynajem
+    '''
+
+def send_rental_invoice_email(customer_email, rental_details):
+    '''
+    Wysyła e-mail z fakturą
+    '''
+
+def generate_daily_report():
+    '''
+    Generuje raport dzienny
+    '''
